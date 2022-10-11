e80016965dfc8ef813d0a3202e7c26f9edfd502a
Ismael Vázquez Fernández <ismavazquezfernandez@gmail.com>
2022-10-11 23:27:14 +0200

    Add logout path and controller

---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LogoutController extends AbstractController
+{
+    public function logout()
+    {
+        
+    }
+}
