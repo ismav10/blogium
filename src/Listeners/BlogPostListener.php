c003b430c5524bb603a0b8c8023750fdaaeafae8
Ismael Vázquez Fernández <ismavazquezfernandez@gmail.com>
2022-10-11 21:30:41 +0200

    Implement listener to add all the necessary properties of the blog post

---
+++ src/Listeners/BlogPostListener.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Entity\BlogPost;
+use App\Service\DataGenerator\FakerFacade;
+use DateTime;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class BlogPostListener
+{
+    private Security $security;
+
+    private FakerFacade $faker;
+
+    private SluggerInterface $slugger;
+
+    /**
+     * An array of images with the photos that can be used as media for the blog posts.
+     * Can be improved getting these routes with the finder component.
+     * Doesn't make sense in a project with real media.
+     */
+    protected const IMAGES = [
+        'images/postphoto1.png',
+        'images/postphoto2.png',
+        'images/postphoto3.png',
+        'images/postphoto4.png',
+        'images/postphoto5.png',
+        'images/postphoto6.png',
+        'images/postphoto7.png',
+    ];
+
+    public function __construct(Security $security, FakerFacade $faker, SluggerInterface $slugger)
+    {
+        $this->security = $security;
+        $this->slugger = $slugger;
+        $this->faker = $faker;
+    }
+
+    /**
+     * Generates all the necessary data for the blog post that is not passed in the request.
+     */
+    public function prePersist(BlogPost $blogPost)
+    {
+        $user = $this->security->getUser();
+
+        if ($user) {
+            $blogPost->setAuthor($user);
+        }
+
+        $slug = $this->slugger->slug($blogPost->getTitle());
+
+        $blogPost->setSlug(strtolower($slug));
+        $blogPost->setMedia($this->faker->execute('randomElement', self::IMAGES));
+        $blogPost->setCreated(new DateTime());
+    }
+}
---
+++ tests/Unit/Listeners/BlogPostListenerTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Tests\Unit\Listeners;
+
+use App\Entity\BlogPost;
+use App\Entity\User;
+use App\Listeners\BlogPostListener;
+use App\Service\DataGenerator\FakerFacade;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
+class BlogPostListenerTest extends TestCase
+{
+    protected function setUp() : void
+    {
+        $this->security = $this->createMock(Security::class);
+        $this->user = $this->createMock(User::class);
+        $this->blogPost = $this->createMock(BlogPost::class);
+        $this->faker = $this->createMock(FakerFacade::class);
+
+        // The returning type of the Slugger mock is causing problems. 
+        // Is because of that that there is a concrete instance here
+        $this->slugger = new AsciiSlugger();
+
+        $this->listener = new BlogPostListener($this->security, $this->faker, $this->slugger);
+    }
+
+    /**
+     * Tests method prePersist when there is an user authenticated.
+     */
+    public function testPrePersistWhenAnUserIsAuthenticated()
+    {
+        $this->security->expects($this->once())
+            ->method('getUser')
+            ->willReturn($this->user);
+
+        $this->blogPost->expects($this->once())
+            ->method('setAuthor')
+            ->with($this->user);
+
+        $this->blogPost->expects($this->once())
+            ->method('getTitle')
+            ->willReturn('Foo bar');
+
+        $this->blogPost->expects($this->once())
+            ->method('setSlug')
+            ->with('foo-bar');
+
+        $this->blogPost->expects($this->once())
+            ->method('setCreated');
+
+        $this->listener->prePersist($this->blogPost);
+    }
+
+    /**
+     * Tests method prePersist when there is no user authenticated.
+     */
+    public function testPrePersistWhenNotUserIsAuthenticated()
+    {
+        $this->security->expects($this->once())
+            ->method('getUser')
+            ->willReturn(null);
+
+        $this->blogPost->expects($this->never())
+            ->method('setAuthor');
+
+        $this->blogPost->expects($this->once())
+            ->method('getTitle')
+            ->willReturn('Foo bar');
+
+        $this->blogPost->expects($this->once())
+            ->method('setSlug')
+            ->with('foo-bar');
+
+        $this->blogPost->expects($this->once())
+            ->method('setCreated');
+
+        $this->listener->prePersist($this->blogPost);
+    }
+}
