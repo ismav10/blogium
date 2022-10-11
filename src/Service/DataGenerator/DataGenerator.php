b5e33dd15cbdcb04411714652b4e5e847bbc97af
Ismael Vázquez Fernández <ismavazquezfernandez@gmail.com>
2022-10-11 20:51:46 +0200

    Add services for random data generation

---
+++ src/Service/DataGenerator/DataGenerator.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Service\DataGenerator;
+
+abstract class DataGenerator 
+{
+    private const INTEGRITY_CONSTRAINT_VIOLATION_CODE = 1062;
+
+    /**
+     * An array of images with the photos that can be used as media for the blog posts.
+     * Can be improved getting these routes with the finder component.
+     * Doesn't make sense in a project with real media.
+     * 
+     * @var array
+     */
+    protected const IMAGES = [
+        'images/postphoto1.png',
+        'images/postphoto2.png',
+        'images/postphoto3.png',
+        'images/postphoto4.png',
+        'images/postphoto5.png',
+        'images/postphoto6.png',
+        'images/postphoto7.png',
+    ];
+
+    /**
+     * The randomly generated users.
+     */
+    protected array $users;
+
+    /**
+     * The randomly generated blog posts.
+     */
+    protected array $blogPosts;
+
+    /**
+     * An instance of the object that passes calls to the third party faker library.
+     */
+    protected $faker;
+
+    public function __construct(FakerFacade $faker)
+    {
+        $this->users = [];
+        $this->blogPosts = [];
+        $this->faker = $faker;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function generate()
+    {
+        $this->generateUsersAndBlogPosts();
+        $this->generateRelationsAndPersist();
+        $this->flush();
+    }
+
+    /**
+     * Flushes the persisted entities to the database.
+     * 
+     * @throws \Exception Throws an exception if there was any problem persisting to the database.
+     */
+    private function flush()
+    {
+        try {
+            $this->entityManager->flush();
+        } catch (\Exception $e) {
+            if ($e->getCode() === self::INTEGRITY_CONSTRAINT_VIOLATION_CODE) {
+                throw new \Exception('There was a constraint violation with the generated data');
+            }
+
+            throw $e;
+        }
+    }
+
+    /**
+     * Randomly generates users and blog posts.
+     */
+    abstract protected function generateUsersAndBlogPosts();
+
+    /**
+     * Randomly generates the relationships between the posts and the authors and calls to persist.
+     */
+    abstract protected function generateRelationsAndPersist();
+}
---
+++ tests/Unit/Service/DataGenerator/JsonPlaceholderDataGeneratorTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Tests\Unit\Service\DataGenerator;
+
+use App\Entity\Blogpost;
+use App\Entity\User;
+use App\Service\DataGenerator\FakerFacade;
+use App\Service\DataGenerator\JsonPlaceholderDataGenerator;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+use function PHPUnit\Framework\throwException;
+
+class JsonPlaceholderDataGeneratorTest extends TestCase
+{
+    private const API_POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+    private const API_USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+
+    protected function setUp() : void
+    {
+        $this->entityManager = $this->createMock(EntityManagerInterface::class);
+        $this->faker = $this->createMock(FakerFacade::class);
+        $this->client = $this->createMock(HttpClientInterface::class);
+        $this->httpException = $this->createMock(HttpException::class);
+        $this->response = $this->createMock(ResponseInterface::class);
+
+        $this->generator = new JsonPlaceholderDataGenerator($this->entityManager, $this->client, $this->faker);
+
+        $this->date = new DateTime();
+
+        $this->user = new User();
+
+        $this->user->setUsername('username');
+        $this->user->setPassword('password');
+        $this->user->setFullname('name');
+
+        $this->blogPost = new Blogpost();
+
+        $this->blogPost->setTitle('sentence');
+        $this->blogPost->setBody('sample text');
+        $this->blogPost->setSlug('slug');
+        $this->blogPost->setCreated($this->date);
+        $this->blogPost->setMedia('images/foo.png');
+        $this->blogPost->setAuthor($this->user);
+
+        $this->fakerResults = [
+            'password',
+            'slug',
+            $this->date,
+            'images/foo.png',
+        ];
+
+        $this->userApiResults = [
+            [
+                'username' => 'username',
+                'name' => 'name',
+            ]
+        ];
+
+        $this->blogApiResults = [
+            [
+                'title' => 'sentence',
+                'body' => 'sample text',
+                'userId' => 1   
+            ]
+        ];
+    }
+
+    /**
+     * Tests generate method when there is a connection error with the API.
+     */
+    public function testGenerateWhenThereIsAConnectionError()
+    {
+        $this->client->expects($this->once())
+            ->method('request')
+            ->with('GET', self::API_USERS_URL)
+            ->will(throwException($this->httpException));
+
+        $this->expectException(\Exception::class);
+
+        $this->generator->generate();
+    }
+
+    /**
+     * Tests generate method when is successful.
+     */
+    public function testGenerateWhenSuccessful()
+    {
+        $this->client->expects($this->exactly(2))
+            ->method('request')
+            ->withConsecutive(
+                [$this->equalTo('GET'), $this->equalTo(self::API_USERS_URL)],
+                [$this->equalTo('GET'), $this->equalTo(self::API_POSTS_URL)],
+            )
+            ->willReturnOnConsecutiveCalls(...[$this->response, $this->response]);
+
+        $this->response->expects($this->exactly(2))
+            ->method('toArray')
+            ->willReturnOnConsecutiveCalls(...[$this->userApiResults, $this->blogApiResults]);
+        
+        $this->faker->expects($this->exactly(4))
+            ->method('execute')
+            ->willReturnOnConsecutiveCalls(...$this->fakerResults);
+
+        $this->entityManager->expects($this->exactly(2))
+            ->method('persist')
+            ->withConsecutive(
+                [$this->equalTo($this->user)],
+                [$this->equalTo($this->blogPost)]
+            );
+
+        $this->generator->generate();
+    }
+}
