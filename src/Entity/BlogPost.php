d4d6bf297ef0d70d851432c72c31cace1a4f1193
Ismael Vázquez Fernández <ismavazquezfernandez@gmail.com>
2022-10-11 19:39:55 +0200

    Add validation for blog posts

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\BlogPostRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: BlogPostRepository::class)]
 class BlogPost
@@ -15,9 +16,19 @@ class BlogPost
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Length(
+        min: 5,
+        minMessage: 'The title must be longer than {{ limit }}',
+        max: 255,
+        maxMessage: 'The title must be shorter than {{ limit }}'
+    )]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\Length(
+        min: 5,
+        minMessage: 'The body must be longer than {{ limit }}'
+    )]
     private ?string $body = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
