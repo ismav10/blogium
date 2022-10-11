e7cb9ccaa614c6deddeebed9561eafa7b0ce4ca8
Ismael Vázquez Fernández <ismavazquezfernandez@gmail.com>
2022-10-11 23:48:17 +0200

    Implement component to show a post item

---
+++ assets/components/show.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="post-inner">
+        <article class="post-content">
+            <div class="post-header">
+                <img class="post-image" :src="post.media" alt="Post image">
+            </div>
+            <h1 class="post-title">
+                {{ post.title }}
+            </h1>
+            <p class="post-body">
+                {{ post.body }}
+            </p>
+            <div class="post-author">
+                <img class="post-author-image" alt="Author's image" src="images/logo.png">
+                <div class="post-author-data">
+                    <p class="post-author-fullname">{{ post.author.fullname }}</p>
+                    <p class="post-date">{{ post.created }}</p>
+                </div>
+            </div>
+        </article>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BlogShowComponent',
+        props: {
+            post: {
+                type: Object,
+                required: true
+            }
+        }
+    };
+</script>
