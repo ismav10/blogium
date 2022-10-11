05299acfaef170a8ca793561d73d857f7f355f80
Ismael Vázquez Fernández <ismavazquezfernandez@gmail.com>
2022-10-11 23:49:16 +0200

    Implement component to show notifications

---
+++ assets/components/notification.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-if="error" class="notification">
+        <p>{{ error }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Notification',
+        props: {
+            error: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
